90c8c0ef51e6d45822408b5c6151330489e336d8
enomoto_ryo <enomoto.ryo@alhinc.jp>
2025-04-17 17:53:07 +0900

    TOP画面・削除処理実装

--- src/main/java/com/example/OKE/service/TaskService.java
+++ src/main/java/com/example/OKE/service/TaskService.java
@@ -3,13 +3,13 @@ package com.example.OKE.service;
 import com.example.OKE.controller.form.TaskForm;
 import com.example.OKE.repository.TaskRepository;
 import com.example.OKE.repository.entity.Task;
+import io.micrometer.common.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Service
@@ -17,39 +17,56 @@ public class TaskService {
     @Autowired
     TaskRepository taskRepository;
 
-    public void saveTask(TaskForm reqTask) throws ParseException {
-        Task saveTask = setTaskEntity(reqTask);
-        taskRepository.save(saveTask);
+    /*
+     * レコード全件取得処理
+     */
+
+    public List<TaskForm> findByLimitDateRange(LocalDate start, LocalDate end, Short status, String content) {
+        LocalDateTime startDateTime;
+        LocalDateTime endDateTime;
+
+        if (start != null) {
+            startDateTime = start.atStartOfDay();
+        } else {
+            startDateTime = LocalDate.of(2020, 1, 1).atStartOfDay();
+        }
+        if (end != null) {
+            endDateTime = end.atTime(23, 59, 59);
+        } else {
+            endDateTime = LocalDate.of(2030, 12, 31).atStartOfDay();
+        }
+        if (status != null && !StringUtils.isEmpty(content)) {
+
+        }
+
+//        List<Task> results = taskRepository.findByLimitDateBetweenOrderByLimitDateDesc(startDateTime, endDateTime);
+        List<Task> results = taskRepository.findAll();
+        List<TaskForm> tasks = setTaskForm(results);
+        return tasks;
     }
 
-    public TaskForm editReport(Integer id) {
-        List<Task> results = new ArrayList<>();
-        results.add((Task) taskRepository.findById(id).orElse(null));
-        List<TaskForm> reports = setTaskForm(results);
-        return reports.get(0);
+    /*
+     * タスク削除
+     */
+    public void deleteTask(Integer id) {
+        taskRepository.deleteById(id);
     }
 
+    /*
+     * DBから取得したデータをFormに設定
+     */
     private List<TaskForm> setTaskForm(List<Task> results) {
         List<TaskForm> tasks = new ArrayList<>();
-
         for (int i = 0; i < results.size(); i++) {
             TaskForm task = new TaskForm();
             Task result = results.get(i);
             task.setId(result.getId());
             task.setContent(result.getContent());
-            task.setCreatedDate(result.getCreatedDate());
+            task.setStatus(result.getStatus());
+            task.setLimitDate(result.getLimitDate());
             tasks.add(task);
         }
         return tasks;
-    }
 
-    /*
-     * リクエストから取得した情報をEntityに設定
-     */
-    private Task setTaskEntity(TaskForm reqTask) throws ParseException {
-        Task task = new Task();
-        task.setId(reqTask.getId());
-        task.setContent(reqTask.getContent());
-        return task;
     }
 }
